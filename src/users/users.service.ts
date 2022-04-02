7e73ae279132ae36cdd3981760fcfe9c66ec6cac
Michal <kolodziejczykmichal269@gmail.com>
2022-04-02 13:42:43 +0200

    create modules

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(@InjectRepository(User) private user: Repository<User>) {}
+
+  create(email: string, password: string) {
+    const user = this.user.create({ email, password });
+
+    return this.user.save(user);
+  }
+
+  findOne(id: number) {
+    return this.user.findOne(id);
+  }
+
+  find(email: string) {
+    return this.user.find({ email });
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('user not found');
+    }
+
+    Object.assign(user, attrs);
+
+    return this.user.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    if (!user) {
+      throw new Error('user not found');
+    }
+
+    return this.user.remove(user);
+  }
+}
